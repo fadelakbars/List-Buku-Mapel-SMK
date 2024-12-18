3e9c234bdf296b2f3b39de1a59d2e685781afd27
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2024-12-19 04:58:29 +0800

    add query for table widget

---
+++ app/Filament/Resources/BukuMapelUmumResource/Widgets/TabelBukuMapelUmum.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Resources\BukuMapelUmumResource\Widgets;
+
+use App\Models\BukuMapelUmum;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class TabelBukuMapelUmum extends BaseWidget
+{
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                BukuMapelUmum::orderBy('judul', 'desc')->take(5)
+            )
+            ->columns([
+                // ...
+                Tables\Columns\TextColumn::make('judul')->label('Judul Buku'),
+                // ->sortable()
+                // ->searchable(),
+                Tables\Columns\TextColumn::make('penerbit')->label('Penerbit'),
+                // ->sortable()
+                // ->searchable(),
+                Tables\Columns\TextColumn::make('kelas')->label('Kelas'),
+                // ->sortable()
+                // ->searchable(),
+                Tables\Columns\TextColumn::make('jumlah')->label('Jumlah'),
+                // ->sortable()
+                // ->searchable(),
+            ]);
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -44,7 +44,7 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 // Widgets\AccountWidget::class,
                 // Widgets\FilamentInfoWidget::class,
-                BukuMapelUmum::class
+                // BukuMapelUmum::class
             ])
             ->middleware([
                 EncryptCookies::class,
