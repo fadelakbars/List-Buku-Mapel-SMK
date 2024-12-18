77d66e1e03568bc38c126df97439cfb6c2975ed4
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2024-12-19 04:16:47 +0800

    update urutan tabel

--- app/Filament/Resources/BukuMapelKejuruanResource.php
+++ app/Filament/Resources/BukuMapelKejuruanResource.php
@@ -29,8 +29,6 @@ class BukuMapelKejuruanResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('judul')->required(),
-                Forms\Components\TextInput::make('penerbit')->required(),
                 Forms\Components\Select::make('jurusan')
                     ->options([
                         'TEKNIK JARINGAN KOMPUTER DAN TELEKOMUNIKASI' => 'TEKNIK JARINGAN KOMPUTER DAN TELEKOMUNIKASI',
@@ -40,6 +38,8 @@ class BukuMapelKejuruanResource extends Resource
                         'DESAIN PERMODELAN DAN INFORMASI BANGUNAN' => 'DESAIN PERMODELAN DAN INFORMASI BANGUNAN',
                         'TEKNIK AUDIO VIDEO' => 'TEKNIK AUDIO VIDEO',
                     ])->required(),
+                Forms\Components\TextInput::make('judul')->required(),
+                Forms\Components\TextInput::make('penerbit')->required(),
                 Forms\Components\Select::make('kelas')
                     ->options([
                         'X' => 'X',
