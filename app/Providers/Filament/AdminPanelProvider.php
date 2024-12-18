ba68600496a33a9caf1bdd4b614af1841f29a28a
Fadhel Akbar <fadhelakbarsallang@gmail.com>
2024-12-19 03:59:15 +0800

    disable breadcrumbs

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             // ->databaseNotifications()
+            ->breadcrumbs(false)
             ->colors([
                 'primary' => Color::Blue,
             ])
